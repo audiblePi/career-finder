1cc3eb4e18e25983dd9a30ff159be87bb127d566
rcaplin <hmbob@ufl.edu>
2020-03-30 12:46:39 -0400

    pushing config for deploy.

---
+++ app/server/config/config.js
@@ -0,0 +1,8 @@
+//This file holds any configuration variables we may need
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+    db: {
+        uri: process.env.MONGODB_URI, //place the URI of your mongo database here.
+    }, 
+};
