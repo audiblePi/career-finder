f10bb66f659a3d3ce31037874e2e64b20735983d
TolerantSnow <60205984+TolerantSnow@users.noreply.github.com>
2020-04-15 16:41:11 -0400

    finishedManageUsers

--- app/client/src/views/ManageUsers/ManageUsers.js
+++ app/client/src/views/ManageUsers/ManageUsers.js
@@ -2,119 +2,97 @@ import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import MaterialTable from 'material-table';
-import axios from 'axios'
+import crypto from 'crypto';
+import axios from 'axios';
 
-const users = [
-    { 
-        id: 1,
-        role: "admin",
-        username: "mars2020",
-        fname: "Perseverance",
-        lname: "Rover",
-        class: "MSL",
-        points: 2020,
-    },
-    { 
-        id: 2,
-        role: "student",
-        username: "msl",
-        fname: "Curiosity",
-        lname: "Rover",
-        class: "MSL",
-        points: 2011,
-    },
-    { 
-        id: 3,
-        role: "student",
-        username: "merB",
-        fname: "Opportunity",
-        lname: "Rover",
-        class: "MER",
-        points: 2003,
-    },
-    { 
-        id: 4,
-        role: "student",
-        username: "merA",
-        fname: "Sprit",
-        lname: "Rover",
-        class: "MER",
-        points: 2003,
-    },
-    { 
-        id: 5,
-        role: "student",
-        username: "pathfinder",
-        fname: "Sojourner",
-        lname: "Rover",
-        class: "SOJ",
-        points: 1997,
-    },
+const users2 = [
+
 ];
 
-const retrieveUsers = () => {
-    return users;
+function getUsers() {
+    axios.get('/_user')
+        .then(res => {
+            return res.data;
+        });
 }
 
+const retrieve = () => {
+    return users2;
+}
+
+
 function ManageUsers() {
+    
+    const [users, setUsers] = React.useState([]);
+
+    React.useEffect(() => {
+        axios.get('/_user')
+        .then((data) => {
+            setUsers(data);
+        });
+    }, []);
+    
     const [state, setState] = React.useState({
-        // columns: [
-        //     { title: 'Name', field: 'name' },
-        //     { title: 'Surname', field: 'surname' },
-        //     { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-        //     { title: 'Birth Place', field: 'birthCity', lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' }, },
-        // ],
-        // data: [
-        //     { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-        //     { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34, },
-        // ],
         columns: [
             { title: 'Username', field: 'username', filtering: false },
             { title: 'FName', field: 'fname', filtering: false  },
             { title: 'LName', field: 'lname', filtering: false  },
-            { title: 'Class', field: 'group', lookup: { 'MSL': 'MSL', 'MER': 'MER' , 'SOJ': 'SOJ'}, },
+            { title: 'Class', field: 'group', filtering: false },
+            { title: 'Password', field: 'password', filtering: false },
             { title: 'Points', field: 'points', filtering: false  },
-            { title: 'Role', field: 'role', lookup: { 'student': 'S', 'admin': 'A'}, },
-        ],
-        data: retrieveUsers()
+            { title: 'Role', field: 'role', lookup: { 'student': 'Student', 'admin': 'Admin'}, },
+        ]
     });
+    
+    const refreshLookupTable = (lookup) => {
+        let newLookup = { title: 'Class', field: 'group', lookup: { }};
+        let set = new Set();
+        for(var k in state.data) {
+            set.add(k);
+        }
+        for(var k in set) {
+            newLookup.lookup.append(k);
+        }
+    }
+    
+    
+    
+    const refresh = () => {
+        axios.get('/_user')
+        .then(res => {
+            setUsers({data: res.data});
+        });
+        //refreshLookupTable();
+    }
 
     const createUser = (user) => {
-        let url = '/_user'
-        axios.post('/_user', user)
+        let url = '/_user';
+        axios.post(url, user);
     }
 
     const updateUser = (user) => {
-        let url = '/_user'
-        axios.put(url, user)
+        let url = '/_user';
+        user.password = crypto.createHash('md5').update(user.password).digest('hex');
+        axios.put(url, user);
     }
     
     const deleteUser = (user) => {
-        let url = '/_user'
-        axios.delete('/_user', {data: user})
+        let url = '/_user';
+        axios.delete(url, {data: user});
     }
     
-    //console.log(state)
-
     return (
+        
         <div>
             <Typography component="h1" variant="h3" color="inherit" gutterBottom>
                 Manage Users
             </Typography>
             
             <MaterialTable
+                
                 title=""
                 columns={state.columns}
-                data={query =>
-                new Promise((resolve, reject) => {
-                    axios.get('/_user')
-                        .then(res => {
-                            resolve({
-                                data: res.data
-                            })
-                        })
-                    })
-                }
+                data={users.data}
                 options={{
                     filtering: true,
                     loadingType: "linear"
@@ -124,7 +102,9 @@ function ManageUsers() {
                         icon: 'refresh',
                         tooltip: 'Refresh Data',
                         isFreeAction: true,
-                        onClick: () => this.tableRef.current && this.tableRef.current.onQueryChange(),
+                        //onClick: () => tableRef.current && tableRef.current.onQueryChange(),
+                        //onClick: () => refreshLookupTable()
+                        onClick: () => refresh()
                     }
                 ]}
                 editable={{
@@ -132,7 +112,7 @@ function ManageUsers() {
                         new Promise(resolve => {
                             setTimeout(() => {
                             resolve();
-                            setState(prevState => {
+                            setUsers(prevState => {
                                 const data = [...prevState.data];
                                 data.push(newData);
 
@@ -147,7 +127,7 @@ function ManageUsers() {
                             setTimeout(() => {
                             resolve();
                             if (oldData) {
-                                setState(prevState => {
+                                setUsers(prevState => {
                                     const data = [...prevState.data];
                                     data[data.indexOf(oldData)] = newData;
 
@@ -162,7 +142,7 @@ function ManageUsers() {
                         new Promise(resolve => {
                             setTimeout(() => {
                             resolve();
-                            setState(prevState => {
+                            setUsers(prevState => {
                                 const data = [...prevState.data];
                                 data.splice(data.indexOf(oldData), 1);
 
